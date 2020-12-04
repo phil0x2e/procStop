3da05a246472d9f6cc66e54323c0b756ace8ec81
phil <ph-ketteni@t-online.de>
2020-12-04 02:22:33 +0100

    php now checks if inputs are empty and reads in the date.

--- web/new_day.php
+++ web/new_day.php
@@ -1,11 +1,26 @@
 <?php
 $tasks = [];
 $times = [];
+
+if (!isset($_POST["date"])){
+   die("Please select date.");
+}
+if (!isset($_POST["task1"]) || !isset($_POST["time1"]) || $_POST["task1"] == "" || $_POST["time1"] == ""){
+    die("Please enter a task with a time.");
+}
+
+$date = $_POST["date"];
 for ($i = 1; ; $i++) {
     if (!isset($_POST["task" . $i]) || !isset($_POST["time" . $i])) {
         break;
     }
-    $tasks[] = $_POST["task" . $i];
-    $times[] = $_POST["time" . $i];
+    $current_task = $_POST["task" . $i];
+    $current_time = $_POST["time" . $i];
+    if ($current_task !== "") {
+        $tasks[] = $current_task;
+    }
+    if ($current_time !== "") {
+        $times[] = $current_time;
+    }
 }
 ?>
