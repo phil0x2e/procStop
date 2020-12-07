a57aebefc8d13be9ad77cc3173b8d46d484bf9b8
phil <ph-ketteni@t-online.de>
2020-12-07 18:26:33 +0100

    Implemented insertion into database.

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,5 +1,7 @@
 FROM php:7-fpm
 LABEL maintainer "Phil"
-COPY web /web
-RUN chmod -R 777 /web
+COPY web /var/www/html
+RUN chmod -R 777 /var/www/html/database.sqlite3
+RUN apt-get update && apt-get upgrade -y
+RUN apt-get install -y sqlite3
 EXPOSE 9000
