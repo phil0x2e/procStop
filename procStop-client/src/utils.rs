e14fbfb1ad30575e5ea4a58a58d250569b4de13e
phil <ph-ketteni@t-online.de>
2020-12-20 21:21:18 +0100

    Forgot to add utils.

---
+++ procStop-client/src/utils.rs
@@ -0,0 +1,11 @@
+pub fn byte_to_bits(n: u8) -> [u8; 8] {
+    let mut array: [u8; 8] = [0; 8];
+    let mut mask = 0x01;
+    for i in (0..8).rev() {
+        if n & mask == mask {
+            array[i] = 1;
+        }
+        mask <<= 1;
+    }
+    array
+}
