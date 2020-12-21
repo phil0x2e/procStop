bb469b52a730750cc2052035d4645dd47b4f7667
phil <ph-ketteni@t-online.de>
2020-12-21 23:56:01 +0100

    Improved readability of byte_to_bits function.

--- procStop-client/src/utils.rs
+++ procStop-client/src/utils.rs
@@ -2,7 +2,7 @@ pub fn byte_to_bits(n: u8) -> [u8; 8] {
     let mut array: [u8; 8] = [0; 8];
     let mut mask = 0x01;
     for i in (0..8).rev() {
-        if n & mask == mask {
+        if n & mask != 0 {
             array[i] = 1;
         }
         mask <<= 1;
